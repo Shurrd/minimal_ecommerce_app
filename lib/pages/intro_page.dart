4924897dfc952c4a7d3ec408665a4f3a2742aa52
Shurrd <engraaadeyemi@gmail.com>
2023-11-13 17:11:31 +0100

    first commit

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,50 @@
+import 'package:ecommerce/components/my_button.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.shopping_bag,
+                size: 75,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                "Minimal Shop",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Premium Quality Products",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              MyButton(
+                onTap: () {},
+                child: const Icon(Icons.arrow_forward),
+              )
+            ],
+          ),
+        ));
+  }
+}
